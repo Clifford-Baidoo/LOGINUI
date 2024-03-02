3a843dfdf50aa2530a1a06784fd2c3e134402a64
Clifford Baidoo <baidooclifford56@gmail.com>
2024-03-02 00:02:27 +0000

    Test

---
+++ lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(MaterialApp(
+    theme: ThemeData(primaryColor: Colors.green),
+    debugShowCheckedModeBanner: false,
+    home: LoginPage(),
+  ));
+}
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+            child: Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+            Color(0xFFF44336),
+            Color(0xFF4DB6AC),
+            Color(0xFF304FFE),
+          ])),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 80,
+          ),
+          Image.asset('images/foxtech-removebg-preview.png'),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            'Foxtech Technologies',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+            ),
+          ),
+          Container(
+            height: 480,
+            width: 325,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(10)),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'Hello',
+                  style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  'Please Login into your account',
+                  style: TextStyle(fontSize: 15, color: Colors.grey),
+                ),
+                Container(
+                    width: 250,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Email Address',
+                        suffixIcon: Icon(
+                          FontAwesomeIcons.envelope,
+                          size: 17,
+                        ),
+                      ),
+                    )),
+                Container(
+                    width: 250,
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        suffixIcon: Icon(
+                          FontAwesomeIcons.eyeSlash,
+                          size: 17,
+                        ),
+                      ),
+                    )),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 40, 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Forgot Password',
+                        style: TextStyle(color: Colors.blueAccent[700]),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 250,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        gradient: LinearGradient(
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
+                            colors: [
+                              Color(0xFFF44336),
+                              Color(0xFF4DB6AC),
+                              Color(0xFF304FFE),
+                            ])),
+                    child: Padding(
+                      padding: EdgeInsets.all(12.0),
+                      child: Text('Login',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold
+                      )
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    )));
+  }
+}
